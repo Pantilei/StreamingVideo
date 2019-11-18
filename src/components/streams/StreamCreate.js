38b6bb64f5a13078f53a1ac90bb2b0601e3fe04d
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-11-18 10:55:21 +0000

    Working with Froms

--- src/components/streams/StreamCreate.js
+++ src/components/streams/StreamCreate.js
@@ -1,7 +1,34 @@
 import React from "react";
+import { Field, reduxForm } from "redux-form";
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  renderInput(formProps) {
+    /* console.log(formProps); */
+    // we take all props and pass them as atributes to input, shorter version
+    return <input {...formProps.input} />;
+    // we take just 2 props
+    /* return (
+      <input
+        onChange={formProps.input.onChange}
+        value={formProps.input.value}
+      />
+    ); */
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form>
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: "streamCreate"
+})(StreamCreate);
