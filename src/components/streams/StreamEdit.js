c3cfab6af4253af001ee6b216af9f223a954eacd
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-11-21 21:30:24 +0000

    Before Code Reuse

--- src/components/streams/StreamEdit.js
+++ src/components/streams/StreamEdit.js
@@ -1,14 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchStream } from "../../actions";
 
 //this props are passed by react-router-dom to this component
-const StreamEdit = props => {
-  console.log(props);
-  return <div>StreamEdit</div>;
-};
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+  }
+
+  render() {
+    if (!this.props.stream) {
+      return <div>Loading...</div>;
+    }
+    console.log(this.props);
+    return <div>{this.props.stream.title}</div>;
+  }
+}
 
 const mapStateToProps = (state, ownProps) => {
   return { stream: state.streams[ownProps.match.params.id] };
 };
 
-export default connect(mapStateToProps)(StreamEdit);
+export default connect(mapStateToProps, { fetchStream })(StreamEdit);
